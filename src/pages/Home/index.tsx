1ff98e7901ff18d7e2ba4ee4eaedeaf65bd2c71d
ChristieYuki <cmashima@ciandt.com>
2023-05-26 15:43:20 -0300

    add header style

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,20 +1,22 @@
 import { Link } from "react-router-dom";
+import { Header } from "../../components/Header";
+
 
 export function Home() {
-    return(
+    return (
         <>
-        <h1>Home</h1>
-        <ul>
-            <li>
-                <Link to='/connect-cat'>Connect Cat</Link>
-            </li>
-            <li>
-                <Link to='/contador'>Contador</Link>
-            </li>
-            <li>
-                <Link to='/to-do'>To Do List</Link>
-            </li>
-        </ul>
+            <Header title="Home" />
+            <ul>
+                <li>
+                    <Link to='/connect-cat'>Connect Cat</Link>
+                </li>
+                <li>
+                    <Link to='/contador'>Contador</Link>
+                </li>
+                <li>
+                    <Link to='/to-do'>To Do List</Link>
+                </li>
+            </ul>
         </>
     )
 }
